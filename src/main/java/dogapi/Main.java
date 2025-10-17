ef7a8bc67e7bd988ae22b274e9a3edff18e80215
CS-Marcus <marcusbharrat1@gmail.com>
2025-10-17 15:59:43 -0400

    Change return value for BreedNotFoundException

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -30,7 +30,7 @@ public class Main {
             int x = breedFetcher.getSubBreeds(breed).size();
             return x;
         } catch (BreedFetcher.BreedNotFoundException e) {
-            return -1;
+            return 0;
         }
     }
 }
